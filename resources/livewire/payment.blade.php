5287a3c935587763d4d7f07147d0fd82922a93da
SteliyanPStoyanov <39989696+SteliyanPStoyanov@users.noreply.github.com>
2024-02-29 11:02:10 +0200

    Update payment.blade.php

--- resources/livewire/payment.blade.php
+++ resources/livewire/payment.blade.php
@@ -9,7 +9,7 @@
         ->setEmailAddress('dddd@ff.bg')
         ->setOrder(123456)
         ->setDescription('test')
-        ->setMerchantUrl(url()) // optional
+        ->setMerchantUrl(url('/')) // optional
         ->setBackRefUrl(url('back-ref-url')) // optional / required for development
         ->setTerminalID('V5402041')
         ->setMerchantId('6210035458')
