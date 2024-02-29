a51d41d07cfe7a8cdef06ed9034825b92ca6317c
SteliyanPStoyanov <39989696+SteliyanPStoyanov@users.noreply.github.com>
2024-02-29 18:07:13 +0200

    Update bulbank.php

--- config/bulbank.php
+++ config/bulbank.php
@@ -2,5 +2,10 @@
 
 return [
     'merchant_id' => env('BULBANK_MERCHANT_ID', ''),
+    'merchant_name' => env('BULBANK_MERCHANT_NAME', ''),
     'terminal_id' => env('BULBANK_TERMINAL_ID', ''),
+    'pass' => env('BULBANK_PASS', ''),
+    'public_key_path' => env('BULBANK_PUBLIC_KEY_PATH', ''),
+    'private_key_path' => env('BULBANK_PRIVATE_KEY_PATH', ''),
+    'private_key_pass' => env('BULBANK_PRIVATE_KEY_PASS', ''),
 ];
