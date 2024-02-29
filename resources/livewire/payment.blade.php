575f5801e57570510394c8783672e1cf74e199bb
SteliyanPStoyanov <39989696+SteliyanPStoyanov@users.noreply.github.com>
2024-02-29 23:43:33 +0200

    Update payment.blade.php

--- resources/livewire/payment.blade.php
+++ resources/livewire/payment.blade.php
@@ -1,24 +1,17 @@
 <div>
         @php
-    $saleRequest = (new Lunar\BulBank\Services\SaleRequest())
-        ->inDevelopment() // set to development
-        ->setAmount(123.32)
-        ->setCountryCode('BG')
-        ->setMerchantName(config('bulbank.merchant_name'))
-        ->setAdCustBorOrderId('22')
-        ->setEmailAddress('dddd@ff.bg')
-        ->setOrder(123456)
-        ->setDescription('test')
-        ->setMerchantUrl(url('/')) // optional
-        ->setBackRefUrl(url('back-ref-url')) // optional / required for development
-        ->setTerminalID(config('bulbank.terminal_id'))
-        ->setMerchantId(config('bulbank.merchant_id'))
-        ->setPrivateKey(base_path(config('bulbank.private_key_path')), config('bulbank.private_key_pass'))
-        ->setSigningSchemaMacGeneral()
-        ->setPrivateKeyPassword(config('bulbank.private_key_pass'));
-
-    $formHtml = $saleRequest->generateForm(); // only generate hidden html form with filled inputs
-
-    @endphp
-    {!!  $formHtml !!}
+            $saleRequest = (new Lunar\BulBank\Services\SaleRequest())
+                   ->setSigningSchemaMacGeneral()
+                   ->setAmount(1.32)
+                   ->setOrder(123456)
+                   ->setDescription('test')
+                   ->setMerchantUrl(url('/')) // optional
+                   ->setBackRefUrl(url('back-ref-url')) // optional / required for development
+                   ->setTerminalID(config('bulbank.terminal_id'))
+                   ->setMerchantId(config('bulbank.merchant_id'))
+                   ->setPrivateKey(base_path(config('bulbank.private_key_path')), config('bulbank.private_key_pass'))
+                   ->setPrivateKeyPassword(config('bulbank.private_key_pass'));
+               $formHtml = $saleRequest->generateForm(); // only generate hidden html form with filled inputs
+        @endphp
+    {!! $formHtml !!}
 </div>
