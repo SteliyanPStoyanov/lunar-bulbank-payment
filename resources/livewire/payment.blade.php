f38930bc8eefeed96e249fbcf4e36c5557217716
SteliyanPStoyanov <39989696+SteliyanPStoyanov@users.noreply.github.com>
2024-03-08 17:25:27 +0200

    Update payment.blade.php

--- resources/livewire/payment.blade.php
+++ resources/livewire/payment.blade.php
@@ -1,18 +1,22 @@
 <div>
-        @php
-            $saleRequest = (new Lunar\BulBank\Services\SaleRequest())
-                   ->inDevelopment() // set to development
-                   ->setSigningSchemaMacGeneral()
-                   ->setAmount(1.32)
-                   ->setOrder(123456)
-                   ->setDescription('test')
-                   ->setMerchantUrl(url('/')) // optional
-                   ->setBackRefUrl(url('back-ref-url')) // optional / required for development
-                   ->setTerminalID(config('bulbank.terminal_id'))
-                   ->setMerchantId(config('bulbank.merchant_id'))
-                   ->setPrivateKey(base_path(config('bulbank.private_key_path')), config('bulbank.private_key_pass'))
-                   ->setPrivateKeyPassword(config('bulbank.private_key_pass'));
-               $formHtml = $saleRequest->generateForm(); // only generate hidden html form with filled inputs
+    @php
+        $saleRequest = (new Lunar\BulBank\Services\SaleRequest())
+               ->setSigningSchemaMacGeneral()
+               ->inDevelopment() // set to development
+               ->setAmount($cart->total->value / 100)
+               ->setMerchantName('ABC PFARMACY LTD')
+               ->setMerchantGMT('+03')
+               ->setCountryCode('BG')
+               ->setOrder($cart->id)
+               ->setDescription('order #'.$cart->id)
+               ->setMerchantUrl(url('/')) // optional
+               ->setBackRefUrl(url('back-ref-url')) // optional / required for development
+               ->setTerminalID(config('bulbank.terminal_id'))
+               ->setMerchantId(config('bulbank.merchant_id'))
+               ->setEmailAddress('amsteljlo1983@gmail.com')
+               ->setPrivateKey(base_path(config('bulbank.private_key_path')), config('bulbank.private_key_pass'))
+               ->setPrivateKeyPassword(config('bulbank.private_key_pass'));
+           $formHtml = $saleRequest->generateForm(); // only generate hidden html form with filled inputs
         @endphp
     {!! $formHtml !!}
 </div>
