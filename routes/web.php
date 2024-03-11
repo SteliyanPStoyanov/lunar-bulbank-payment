cfaadda377defc8eff4cc9260f67ab399c4d81d5
SteliyanPStoyanov <39989696+SteliyanPStoyanov@users.noreply.github.com>
2024-03-11 14:51:48 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Lunar\BulBank\Http\Controllers\WebhookController;
+use Lunar\BulBank\Http\Controllers\CheckStatusController;
 
 Route::post('/back-ref-url', WebhookController::class);
+Route::get('/check-status/{order}', CheckStatusController::class);
