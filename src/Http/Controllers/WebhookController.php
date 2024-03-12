1bf1c2c8bf0749441d27af3b23e4d13ba1d2c6d0
Steliyan Stoyanov <steliyan.p.stoyanov@gmail.com>
2024-03-12 10:30:40 +0200

    bulbank response of payment

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -4,17 +4,40 @@ namespace Lunar\BulBank\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Lunar\Base\DataTransferObjects\PaymentAuthorize;
+use Lunar\BulBank\Exceptions\ParameterValidationException;
+use Lunar\BulBank\Exceptions\SendingException;
+use Lunar\BulBank\Exceptions\SignatureException;
 use Lunar\BulBank\Services\SaleResponse;
+use Lunar\Facades\CartSession;
+use Lunar\Facades\Payments;
 
 final class WebhookController extends Controller
 {
+
+
+    /**
+     * @throws SignatureException
+     * @throws SendingException
+     * @throws ParameterValidationException
+     */
     public function __invoke(Request $request)
     {
         $saleResponse = (new SaleResponse())
             ->setPublicKey(base_path(config('bulbank.public_cer_path')))
             ->setSigningSchemaMacGeneral(); // use MAC_GENERAL
 
-        dd($request, $saleResponse, $saleResponse->getResponseData(false));
+        $responseData = $saleResponse->getResponseData(false);
+
+        if ($responseData['RC'] === 0) {
+            Payments::driver('bulbank')->cart(CartSession::current())->withData(array_merge([
+                'ip' => app()->request->ip(),
+                'accept' => app()->request->header('Accept'),
+            ]))->authorize();
+        } else {
+            CartSession::forget();
+            throw new SendingException(json_encode($responseData));
+        }
 
     }
 }
