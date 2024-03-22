9511d97ff8115b2c73d5c3ca309e91a424c66490
Steliyan Stoyanov <steliyan.p.stoyanov@gmail.com>
2024-03-22 16:57:53 +0200

    add terms check validation

--- resources/livewire/payment.blade.php
+++ resources/livewire/payment.blade.php
@@ -1,22 +1,46 @@
 <div>
-    @php
-        $saleRequest = (new Lunar\BulBank\Services\SaleRequest())
-               ->setSigningSchemaMacGeneral()
-               ->inDevelopment() // set to development
-               ->setAmount($cart->total->value / 100)
-               ->setMerchantName('ABC PFARMACY LTD')
-               ->setMerchantGMT('+03')
-               ->setCountryCode('BG')
-               ->setOrder($cart->id)
-               ->setDescription('order #'.$cart->id)
-               ->setMerchantUrl(url('/')) // optional
-               ->setBackRefUrl(url('back-ref-url')) // optional / required for development
-               ->setTerminalID(config('bulbank.terminal_id'))
-               ->setMerchantId(config('bulbank.merchant_id'))
-               ->setEmailAddress('amsteljlo1983@gmail.com')
-               ->setPrivateKey(base_path(config('bulbank.private_key_path')), config('bulbank.private_key_pass'))
-               ->setPrivateKeyPassword(config('bulbank.private_key_pass'));
-           $formHtml = $saleRequest->generateForm(); // only generate hidden html form with filled inputs
-        @endphp
-    {!! $formHtml !!}
+    <form wire:submit.prevent="send">
+        <div class="p-4 text-sm text-center text-blue-700 rounded-lg bg-blue-50">
+            {{ __('checkout.payment-is-bul-bank') }}
+        </div>
+        <div class="mt-3">
+            <label for="checkbox-1">
+                <input type="checkbox" wire:model="terms" name="terms" value="1">
+                <span class="">
+Съгласен съм с <a target="_blank" href="/pages/terms" title="" class="underline">общите условия</a>
+</span>
+            </label>
+            <br/>
+            @if($errors->has('terms'))
+                <strong class="text-sm font-medium text-red-600">{{ $errors->first('terms') }}</strong>
+            @endif
+        </div>
+        <button
+            class="px-5 py-3 mt-4 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-500"
+            type="submit" wire:key="send_submit_btn">
+                        <span wire:loading.remove.delay wire:target="send">
+                            {{ __('checkout.submit-order') }}
+                        </span>
+            <span wire:loading.delay wire:target="send">
+                            <svg class="w-5 h-5 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                 viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                                </path>
+                            </svg>
+                        </span>
+        </button>
+    </form>
+    <div class="hidden">
+        {{-- Conditionally render the Borica form and auto-submit it --}}
+        @if ($boricaFormHtml)
+            <div x-data x-init="document.getElementById('borica3dsRedirectForm').submit()">
+                {!! $boricaFormHtml !!}
+            </div>
+        @endif
+    </div>
+
+
 </div>
--- src/Http/Livewire/Payment.php
+++ src/Http/Livewire/Payment.php
@@ -3,12 +3,17 @@
 namespace Lunar\BulBank\Http\Livewire;
 
 use Livewire\Component;
+use Lunar\BulBank\Exceptions\ParameterValidationException;
+use Lunar\BulBank\Exceptions\SignatureException;
+use Lunar\BulBank\Services\SaleRequest;
 use Lunar\Models\Cart;
 
 class Payment extends Component
 {
 
     public Cart $cart;
+    public $terms = false;
+    public $boricaFormHtml = '';
 
 
     /**
@@ -17,5 +22,35 @@ class Payment extends Component
     public function render()
     {
         return view('bulbank::livewire.payment');
+    }
+
+    /**
+     * @throws SignatureException
+     * @throws ParameterValidationException
+     */
+    public function send(): void
+    {
+        if ($this->terms === false) {
+            $this->addError('terms', 'Полето а задължително !');
+        } else {
+            $saleRequest = (new SaleRequest())
+                ->setSigningSchemaMacGeneral()
+                ->inDevelopment() // set to development
+                ->setAmount($this->cart->total->value / 100)
+                ->setMerchantName('ABC PFARMACY LTD')
+                ->setMerchantGMT('+03')
+                ->setCountryCode('BG')
+                ->setOrder($this->cart->id)
+                ->setDescription('order #' . $this->cart->id)
+                ->setMerchantUrl(url('/')) // optional
+                ->setBackRefUrl(url('back-ref-url')) // optional / required for development
+                ->setTerminalID(config('bulbank.terminal_id'))
+                ->setMerchantId(config('bulbank.merchant_id'))
+                ->setEmailAddress('amsteljlo1983@gmail.com')
+                ->setPrivateKey(base_path(config('bulbank.private_key_path')), config('bulbank.private_key_pass'))
+                ->setPrivateKeyPassword(config('bulbank.private_key_pass'));
+
+             $this->boricaFormHtml = $saleRequest->generateForm();
+        }
     }
 }
